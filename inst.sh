0892b7e6ac29aabc81f49294b0f13c3658c12116
Aleksei <aleksei.kotliarov@epita.fr>
2024-08-18 15:18:57 +0200

    install

---
+++ inst.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+brew install --cask nikitabobko/tap/aerospace
+brew install kitty zsh starship tmux lsd
+brew install --HEAD neovim
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
